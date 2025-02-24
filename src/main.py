b20cc267f06a31768592780237590dd32bacec3f
aCup_of_Tea <adenisov810@gmail.com>
2025-02-25 00:49:37 +0300

    use run settings + include api router to main app

--- src/main.py
+++ src/main.py
@@ -1,10 +1,29 @@
+import uvicorn
+
 from fastapi import FastAPI
+
+from config import settings
+from api import router as api_router
+
 
 app = FastAPI(
     title="ASAO Main API",
+)
+
+app.include_router(
+    api_router,
 )
 
 
 @app.get("/")
 def say_hello():
     return {"message" : "Hello, ASAO!"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host=settings.run.host,
+        port=settings.run.port,
+        reload=True,
+    )
