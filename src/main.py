566e18dae1f1d539af6ac5fff411400e4c63c73d
aCup_of_Tea <adenisov810@gmail.com>
2025-02-27 23:33:29 +0300

    add db helper dispose to main app

--- src/main.py
+++ src/main.py
@@ -1,13 +1,25 @@
+from contextlib import asynccontextmanager
 import uvicorn
 
 from fastapi import FastAPI
 
 from config import settings
 from api import router as api_router
+from database import db_helper
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    yield
+    # shutdown
+    print("dispose engine")
+    await db_helper.dispose()
 
 
 app = FastAPI(
     title="ASAO Main API",
+    lifespan=lifespan,
 )
 
 app.include_router(
