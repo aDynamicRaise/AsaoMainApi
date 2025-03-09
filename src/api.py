af4243d9dabc2ec22a8610517170a13e895e52f0
aCup_of_Tea <adenisov810@gmail.com>
2025-03-09 17:23:12 +0300

    include user router for api

--- src/api.py
+++ src/api.py
@@ -1,8 +1,14 @@
 from fastapi import APIRouter
 
 from config import settings
+from auth.routers import router as router_users
 
 
 router = APIRouter(
     prefix = settings.api.prefix,
+)
+
+router.include_router(
+    router_users,
+    prefix=settings.api.users, 
 )
