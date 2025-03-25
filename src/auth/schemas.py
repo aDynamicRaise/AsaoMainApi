b1579bf486bbf8d335465238ecca45c5f48bed2e
aCup_of_Tea <adenisov810@gmail.com>
2025-03-25 18:16:43 +0300

    update pydentic schemas for user

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -1,22 +1,49 @@
+import re
+from fastapi import status
 from datetime import datetime
-from pydantic import BaseModel, EmailStr, Field, field_validator
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel, EmailStr, Field, ValidationError, field_validator
 
 
-class UserLogin(BaseModel):
-    email: EmailStr
+class UserBase(BaseModel):
+    email: str
+
+    @field_validator('email')
+    def validate_email(cls, value):
+        # Используем регулярное выражение, чтобы проверить формат email 
+        email_regex = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+        
+        if not re.match(email_regex, value):
+            raise ValueError("Неверный формат адреса электронной почты. Адрес должен быть по типу: user@example.com")
+        return value
+
+class UserLogin(UserBase):
     password: str = Field(max_length=20)
-
+        
     @field_validator('password')
-    def validate_password(cls, v):
-        assert len(v) >= 4, 'password must be at least 4 characters'
-        return v
+    def validate_password(cls, value):
+        value = str(value) 
+        if len(value) < 4:
+            raise ValueError("Пароль должен содержать не менее 4 символов")
+        if len(value) > 20:
+            raise ValueError("Пароль должен содержать не более 20 символов")
+        return value
 
 
-class UserCreate(BaseModel):
+class UserCreate(UserBase):
     name: str = Field(max_length=20)
-    email: EmailStr
+
 
 
 class UserRead(UserCreate):
     id: int
 
+
+class ResponceCreate(BaseModel):
+    user_id: int
+
+
+class UserPassSchema(BaseModel):
+    user_id: int
+    hash_pass: str = Field(min_length=60)
+    date_pass: datetime
