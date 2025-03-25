e39fe9442207829b677b2cc3dd31177775bd1d60
aCup_of_Tea <adenisov810@gmail.com>
2025-03-25 15:00:22 +0300

    add middleware cors

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import uvicorn
 
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 from config import settings
 from api import router as api_router
@@ -28,6 +29,14 @@ app.include_router(
     api_router,
 )
 
+origins = [ "http://localhost:3000", ]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def say_hello():
